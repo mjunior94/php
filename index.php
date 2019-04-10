7d5c708af4a4c2170e7829708fca142fb2da26b7
Moacir Junior <jmoacirjunior@gmail.com>
2019-04-10 12:19:10 -0300

    mudei o primeiro echo e add outro

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "Meu primeiro código PHP! <strong>Olá Mundo</strong>";
+echo "Meu primeiro código PHP! <strong>Olá Mundo 456</strong>";
+
+echo "Meu primeiro código PHP! <strong>Olá Udemy</strong>";
 
 ?>
